2d79f149bc08f3828279ed26f0b7c6bcd8edac47
José Rodrigo Roa Acosta <jroa@cosinte.bog>
2024-11-21 15:20:08 -0500

    Segunda clase,falta el color al llegar a un numero determinado

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import imagen0 from './imagenes/rey_atanagildo.png';
+import imagen1 from './imagenes/rey_sisebuto.png';
+import { useRef } from 'react';
+
+import './App.css'
+
+function App() {
+  const cambio = 23.16;
+  const refCaja = useRef();
+
+  const incrementar = (e) => {
+    e.target.innerHTML = Number(e.target.innerHTML) + 1;
+    if (e.target.innerHTML >= 10) {
+      e.target.innerHTML=1;
+    }
+  }
+
+  const convertir = () => {
+    refCaja.current.innerHTML = Number(refCaja.current.innerHTML) * cambio;
+  }
+
+  const cambiar = (j) => {
+    if (j.target.src.includes("atanagildo")) {
+      j.target.src=imagen1;
+    } else {
+      j.target.src=imagen0;
+    }
+      
+    
+  }
+
+
+const lectura = (e) => {
+  refCaja.current.innerHTML = e.target.value;
+}
+
+return (
+  <>
+    <div ref={refCaja} className='caja' onClick={incrementar}>1</div>
+    <button onClick={convertir}>Aceptar</button>
+    <div><img onClick={cambiar} src={imagen0} /></div>
+    <input onChange={lectura} className="campo" />
+
+  </>
+)
+}
+
+export default App;
