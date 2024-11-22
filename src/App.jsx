1d5042e18d751fa231d9219945f9508425bad625
jose <rodrigoroa042@gmail.com>
2024-11-22 16:40:24 -0500

    Segunda clase con retos o.k.

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,14 @@ function App() {
   const incrementar = (e) => {
     e.target.innerHTML = Number(e.target.innerHTML) + 1;
     if (e.target.innerHTML >= 10) {
-      e.target.innerHTML=1;
+      e.target.innerHTML = 1;
     }
+    if (e.target.innerHTML >=8) {
+      e.target.style.backgroundColor = "red";
+  } else{
+    e.target.style.backgroundColor = "green";
   }
+}
 
   const convertir = () => {
     refCaja.current.innerHTML = Number(refCaja.current.innerHTML) * cambio;
@@ -21,28 +26,28 @@ function App() {
 
   const cambiar = (j) => {
     if (j.target.src.includes("atanagildo")) {
-      j.target.src=imagen1;
+      j.target.src = imagen1;
     } else {
-      j.target.src=imagen0;
+      j.target.src = imagen0;
     }
-      
-    
+
+
   }
 
 
-const lectura = (e) => {
-  refCaja.current.innerHTML = e.target.value;
-}
+  const lectura = (e) => {
+    refCaja.current.innerHTML = e.target.value;
+  }
 
-return (
-  <>
-    <div ref={refCaja} className='caja' onClick={incrementar}>1</div>
-    <button onClick={convertir}>Aceptar</button>
-    <div><img onClick={cambiar} src={imagen0} /></div>
-    <input onChange={lectura} className="campo" />
+  return (
+    <>
+      <div ref={refCaja} className='caja' onClick={incrementar}>1</div>
+      <button onClick={convertir}>Aceptar</button>
+      <div><img onClick={cambiar} src={imagen0} /></div>
+      <input onChange={lectura} className="campo" />
 
-  </>
-)
+    </>
+  )
 }
 
 export default App;
